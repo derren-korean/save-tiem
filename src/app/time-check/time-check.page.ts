9264f770595ccd0870f877094d18466a23172dd7
derren-korean <derren832@gamil.com>
2021-04-16 21:10:13 +0900

    add filter function
    UI에서 특정 recoder group만 볼 수 있도록 filter하는 기능 추가

--- src/app/time-check/time-check.page.ts
+++ src/app/time-check/time-check.page.ts
@@ -1,22 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { IonButton } from '@ionic/angular';
 import { RecoderGroup } from './recoder-group.model';
 import { TimeCheckService } from './time-check.service';
+
+const FILTERED: string = "solid"
 
 @Component({
   selector: 'app-time-check',
   templateUrl: './time-check.page.html',
   styleUrls: ['./time-check.page.scss'],
 })
+
 export class TimeCheckPage implements OnInit {
 
-  recoderGroups:RecoderGroup[];
+  filteredGroups: RecoderGroup[];
+  private recoderGroups: RecoderGroup[];
   private _TIME_TYPE:string[] = ['check', 'save']
   constructor(private tcService: TimeCheckService) {}
 
   ngOnInit() {
     this.tcService.fetchRecoders().subscribe(recoders => {
       this.recoderGroups = recoders;
+      this.filteredGroups = [...this.recoderGroups];
     });
   }
 
+  // 선택한 state만 보기!
+  // default: a, b 둘다 보인다. (반대로 둘다 안보이는 상황은 없다)
+  // a를 클릭하면, b를 hide.(반대도 동일하게 1개만 선택가능)
+  // a를 다시 누르면 a,b 둘다 보인다.
+  // b가 hide 상태에서 b을 누를 수 없도록 disabled한다. (반대도 동일함)
+  // 클릭시 fill은 "solid" -> "outline"으로 변경 된다.
+  filterGroups(sibling: IonButton, isDayTime: boolean) {
+    this._changeButtonState(sibling)
+    this._filterByDayTime(sibling.fill == FILTERED, isDayTime)
+  }
+
+  _changeButtonState(button: IonButton) {
+    button.disabled = !button.disabled;
+    button.fill = button.fill == "outline" ? "solid" : "outline"; // 하.. enum 쓰고싶다 ㅠㅠ
+  }
+
+  _filterByDayTime(reset: boolean, isDayTime: boolean) {
+    if (reset) {
+      this._reset();
+    } else {
+      this.filteredGroups = this.recoderGroups.filter(recoder => recoder.isDayTime != isDayTime);
+    }
+  }
+
+  _reset() {
+    this.filteredGroups = [...this.recoderGroups];
+  }
 }
--- src/app/time-check/time-check.service.ts
+++ src/app/time-check/time-check.service.ts
@@ -1,17 +1,16 @@
 import { Injectable, OnInit } from '@angular/core';
-
-import { Recoder } from './recoder.model';
+import { StationInfo } from './station-info';
 import { RecoderGroup } from './recoder-group.model';
 import { HttpClient } from '@angular/common/http';
-import { EventEmitter } from 'events';
 import { map } from 'rxjs/operators';
 
 interface JsonData {
   data: InnerData[];
 }
+
 interface InnerData {
   location: string;
-  total: number;
+  info: StationInfo;
 }
 
 @Injectable({
@@ -30,24 +29,15 @@ export class TimeCheckService {
     .pipe(
       map(res => {
         res.data.forEach((_data)=> {
-          this._setGroupMap(_data.location, _data.total);
+          this._setGroupMap(_data.location, _data.info);
         });
         return [...this.recoders];
       })
     );
   }
 
-  _setGroupMap(location: string, stationNumber: number) {
-    this.recoders.push(
-      new RecoderGroup(location, this._getOrderdRecoders(location, stationNumber))
-    );
-  }
-
-  _getOrderdRecoders(location:string, number:number):Recoder[] {
-    const _temp:Recoder[] = [];
-    while(0 < number) {
-      _temp.push(new Recoder(location, ""+number--, null, null));
-    }
-    return _temp.reverse();
+  _setGroupMap(location: string, info: StationInfo) {
+    if (!info.station.length) return
+    this.recoders.push(new RecoderGroup(location, info));
   }
 }
