0786df383ab003ca81e7294d0ce6a7bbd2478519
derren-korean <derren832@gamil.com>
2023-11-14 08:44:40 +0900

    update functions
    add showing for locations, sawp order

--- src/app/time-check/time-write/time-write.component.ts
+++ src/app/time-check/time-write/time-write.component.ts
@@ -11,13 +11,36 @@ export class TimeWriteComponent implements OnInit {
   @Input() filteredGroups: RecoderGroup[];
   @Output() save = new EventEmitter<SaveData>();
   _TIME_TYPE:string[] = ['check', 'save'];
+  showLoations:string[] = [];
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
+
+  toggle(event: any, location:string) {
+    if(event.detail.checked) {
+      this.showLoations.push(location)
+    } else {
+      this.showLoations = this.showLoations.filter(_lo => _lo != location);
+    }    
+  }
+
+  show(location: string) {
+    return !!this.showLoations.find(lo => lo == location);
+  }
+  
+  reverseOrder(location: string) {
+    this.filteredGroups.forEach(group => {
+      if(group.location == location) {
+        group.recoders = group.recoders.reverse();
+      }
+    })
+  }
 
   onSave(event: any, groupIdx: number, recoderIdx: number, prop:string) {
     if (!event.keyCode) return;
     const time = event.target.value
     this.save.emit({groupIdx, recoderIdx, prop, time});
   }
+
 }
